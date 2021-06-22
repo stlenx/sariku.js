704dd57511f66a406ff10e488878062e4aee0ab2
stlenx <69406526+stlenx@users.noreply.github.com>
2021-06-22 17:39:20 +0200

    some new functions and example file

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let squareX = 0;
+let circleX = 0;
 
 function frame(dt) {
     let size = canvas.height * 0.1; //10% of the screen
     ctx.fillRect(squareX, canvas.height/2, size, size);
+    FillCircle(new Vector2(circleX, size), size);
 
     squareX += 10 * (dt/16);
+    circleX += 5 * (dt/16);
     if(squareX > canvas.width) squareX = 0;
+    if(circleX > canvas.width) circleX = 0;
 }
--- sariku.js
+++ sariku.js
@@ -30,3 +30,71 @@ function animationFrame() {
 if (typeof frame == 'function') {
     window.requestAnimationFrame(animationFrame)
 }
+
+function StrokeLine(p1, p2) {
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function StrokeCircle(p, r) {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, r, 0, Math.PI*2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function FillCircle(p, r) {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, r, 0, Math.PI*2);
+    ctx.fill();
+    ctx.closePath();
+}
+
+class Vector2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Length() {
+        return Math.sqrt((this.x * this.x) + (this.y * this.y))
+    }
+
+    normalize() {
+        let h = this.Length()
+        this.x /= h;
+        this.y /= h;
+    }
+
+    ReturnNormalized() {
+        let h = this.Length()
+        return new Vector2(this.x / h, this.y / h)
+    }
+
+    mult(vector) {
+        this.x *= vector.x;
+        this.y *= vector.y;
+    }
+
+    add(vector) {
+        this.x += vector.x;
+        this.y += vector.y;
+    }
+
+    minus(vector) {
+        this.x -= vector.x;
+        this.y -= vector.y;
+    }
+
+    Scale(amount) {
+        this.x *= amount;
+        this.y *= amount;
+    }
+
+    ReturnScaled(amount) {
+        return new Vector2(this.x * amount, this.y * amount)
+    }
+}
